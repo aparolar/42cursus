cf8dd509e249148a3082ba58942bc1c34ff4149f
aparolar <82216543+aparolar@users.noreply.github.com>
2021-04-19 19:11:55 +0200

    push

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/17 16:39:29 by aparolar          #+#    #+#             */
-/*   Updated: 2021/04/19 13:06:12 by aparolar         ###   ########.fr       */
+/*   Updated: 2021/04/19 19:06:04 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static t_uint	ft_find_chars(char *str, char c)
 {
 	t_uint	len;
 
-	len = 0;
+	len = 1;
 	while (*str)
 	{
 		if (*str == c || !*(str + 1))
@@ -26,21 +26,72 @@ static t_uint	ft_find_chars(char *str, char c)
 	}
 	return (len);
 }
-
+/*
 static char	*ft_get_substr_begins(char *str, char c)
 {
 	while (*str && *str == c)
 		str++;
 	return (str);
 }
-
+*/
+/*
 static char	*ft_get_substr_ends(char *str, char c)
 {
 	while (*str && *str != c)
 		str++;
 	return (str);
 }
+*/
 
+static char	**ft_get_split(char **split, char *s, char c)
+{
+	int		i;
+	char	*s2;
+	char	*s1;
+
+	s1 = s;
+	s2 = s1;
+	i = 0;
+	while (*s1)
+	{
+		while (*s1 == c)
+			s1++;
+		s2 = s1;
+		while (*s2 && *s2 != c)
+			s2++;
+		if (s1 == s2)
+			break ;
+		split[i] = ft_calloc((s2 - s1 + 1), sizeof(char));
+		ft_memcpy(split[i], s1, s2 - s1);
+		s1 = s2;
+		i++;
+	}
+	if (i == 0)
+		return (NULL);
+	split[i] = NULL;
+	return (split);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	t_uint	maxi;
+	char	**split;
+	char	*s2;
+	char	*s1;
+
+	if (!s)
+		return (0);
+	s1 = (char *)s;
+	s2 = s1;
+	maxi = ft_find_chars(s1, c);
+	if (!maxi)
+		return (0);
+	split = ft_calloc(maxi, sizeof(char *));	
+	if (!split)
+		return (0);
+	return (ft_get_split(split, (char *)s, c));
+}
+/*
 char	**ft_split(char const *s, char c)
 {
 	t_uint	maxi;
@@ -56,7 +107,7 @@ char	**ft_split(char const *s, char c)
 	maxi = ft_find_chars(s1, c);
 	if (!maxi)
 		return (0);
-	split = ft_calloc(maxi, sizeof(char *));
+	split = ft_calloc(maxi, sizeof(char **));
 	if (!split)
 		return (0);
 	i = 0;
@@ -64,10 +115,10 @@ char	**ft_split(char const *s, char c)
 	{
 		s1 = ft_get_substr_begins(s2, c);
 		s2 = ft_get_substr_ends(s1, c);
-		if (i <= maxi)
-			split[i] = ft_substr(s1, 0, (t_uint)(s2 - s1));
+		split[i] = ft_substr(s1, 0, (t_uint)(s2 - s1));
 		s2++;
 		i++;
 	}
 	return (split);
 }
+*/
--- libft/mmain.c
+++ libft/mmain.c
@@ -4,25 +4,25 @@ int	main(void)
 {
 	char	**str;
 	int		i;
-
+/*
 	str = ft_split("      split       this for   me  !", ' ');
 	i = 0;
-	while (ft_strncmp(str[i], "", ft_strlen(str[i])))
+	while (str[i])
 	{
 		printf("%s\n", str[i]);
 		i++;
 	}
-	str = 0;
-	str = ft_split("split  ||this|for|me|||||!|", '|');
+*/
+	str = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse", ' ');
 	i = 0;
-	while (ft_strncmp(str[i], "", ft_strlen(str[i])))
+	while (str[i])
 	{
 		printf("%s\n", str[i]);
 		i++;
 	}
-	str = ft_split("                  olol", ' ');
+	str = ft_split("  tripouille  42", 0);
 	i = 0;
-	while (str && ft_strncmp(str[i], "", ft_strlen(str[i])))
+	while (str && str[i])
 	{
 		printf("%s\n", str[i]);
 		i++;
