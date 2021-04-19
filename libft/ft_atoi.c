cbf771509807450b4f971a72907a5df3a1b15e93
alberto <alberto.parolari.blasi@gmail.com>
2021-04-20 00:01:46 +0200

    pusho

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 00:10:39 by aparolar          #+#    #+#             */
-/*   Updated: 2021/04/19 23:48:51 by aparolar         ###   ########.fr       */
+/*   Updated: 2021/04/19 23:57:23 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ int	ft_atoi(const char *str)
 	}
 	while (ft_isdigit((int)*str))
 	{
-		if (num * sign > INT_MAX/* 2147483647*/)
+		if (num * sign > INT_MAX)
 			return (-1);
-		else if (num * sign < INT_MIN/*-2147483648*/)
+		else if (num * sign < INT_MIN)
 			return (0);
 		num = (num * 10) + *str - 48;
 		str++;
