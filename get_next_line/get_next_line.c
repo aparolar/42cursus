702211fbf0765730ebd0a037063c3cad126d2884
alberto <alberto.parolari.blasi@gmail.com>
2021-04-29 22:25:56 +0200

    bla

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 12:17:56 by aparolar          #+#    #+#             */
-/*   Updated: 2021/04/29 21:30:17 by aparolar         ###   ########.fr       */
+/*   Updated: 2021/04/29 22:24:43 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,7 @@ size_t	get_line(char *buff, size_t bytes, char **line)
 		while (*pebuff && *pebuff != '\n' && pebuff - buff < BUFFER_SIZE)
 		{
 			if (*pebuff == '\n' || *(pebuff + 1) == '\n')
-			{
 				*((int *)(buff + BUFFER_SIZE + sizeof(int))) = 1;
-				break ;
-			}
 			pebuff++;
 		}
 		new = malloc((ft_strlen(*line) + pebuff - pbuff) * sizeof(char));
@@ -55,8 +52,6 @@ int	get_next_line(int fd, char **line)
 	*ret = 0;
 	if (fd)
 	{
-		//if (*line)
-		//	free(*line);
 		*line = malloc(sizeof(char));
 		while (*ret != EOF)
 		{
