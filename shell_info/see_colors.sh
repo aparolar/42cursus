11c235178bbac3cd73890f1039456689c05b3e91
alberto <alberto.parolari.blasi@gmail.com>
2021-05-18 18:45:10 +0200

    New updates

--- shell_info/see_colors.sh
+++ shell_info/see_colors.sh
@@ -6,12 +6,15 @@ do
 done;
 echo;
 echo -en "Styles\n";
-for OPT1 in {1..1}
+for OPT0 in {1..38}
 do
-	for OPT2 in {1..47};
+	for OPT1 in {1..20}
 	do
-		echo -en "\033[${OPT};${OPT2}m${OPT1}-${OPT2} ";
-		tput sgr0;
+		for OPT2 in {1..47};
+		do
+			echo -en "\033[${OPT0};${OPT1};${OPT2}m[${OPT0}-${OPT1}-${OPT2}] ";
+			tput sgr0;
+		done;
 	done;
 done;
 echo;
