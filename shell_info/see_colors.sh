65db6815e7cc78c1f3c3ba579c2aaed01b03d926
aparolar <aparolar@student.42madrid.com>
2021-05-19 11:04:51 +0200

    Updates

--- shell_info/see_colors.sh
+++ shell_info/see_colors.sh
@@ -8,13 +8,14 @@ echo;
 echo -en "Styles\n";
 for OPT0 in {1..38}
 do
-	for OPT1 in {1..20}
+	for OPT1 in {1..9}
 	do
 		for OPT2 in {1..47};
 		do
 			echo -en "\033[${OPT0};${OPT1};${OPT2}m[${OPT0}-${OPT1}-${OPT2}] ";
 			tput sgr0;
 		done;
+		#sleep 5;
 	done;
 done;
 echo;
